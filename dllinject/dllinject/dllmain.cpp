f2576d78ff78f58fcdcbb6b6356203c7322efb81
Sam Peddamatham <peddamat@gmail.com>
2022-06-03 11:48:45 -0700

    Checkpoint.

--- dllinject/dllinject/dllmain.cpp
+++ dllinject/dllinject/dllmain.cpp
@@ -1,45 +1,141 @@
 #include "stdafx.h"
 #include <stdio.h>
 #include <windows.h>
+#include "dllmain.h"
 
+HWND m_hwnd = NULL;
+DWORD m_pid = 0;
+LONG OldWndProc;
+
+DWORD WINAPI init(LPVOID);
+DWORD WINAPI deinit(LPVOID);
+LRESULT CALLBACK NewWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 INT APIENTRY DllMain(HMODULE hDLL, DWORD Reason, LPVOID Reserved) {
-    /* open file */
-    FILE *file;
-    fopen_s(&file, "temp.txt", "a+");
+	/* open file */
+	FILE* file;
+	fopen_s(&file, "c:\\users\\me\\temp.txt", "a+");
 
-    switch(Reason) {
-    case DLL_PROCESS_ATTACH:
-                fprintf(file, "DLL attach function called.\n");
-        break;
-    case DLL_PROCESS_DETACH:
-                fprintf(file, "DLL detach function called.\n");
-        break;
-    case DLL_THREAD_ATTACH:
-                fprintf(file, "DLL thread attach function called.\n");
-        break;
-    case DLL_THREAD_DETACH:
-                fprintf(file, "DLL thread detach function called.\n");
-        break;
-    }
+	switch (Reason) {
+	case DLL_PROCESS_ATTACH:
+	{
+		fprintf(file, "DLL attach function called.\n");
+		//CreateThread(0, NULL, (LPTHREAD_START_ROUTINE)&init, NULL, NULL, NULL);
 
-    /* close file */
-    fclose(file);
+		//Find the window of the Injectee using its title
+		HWND hwnd = FindWindow(L"Notepad", NULL);
 
-    return TRUE;
+		//If the window found, then change it's window proc
+		if (hwnd)
+		{
+			fprintf(file, "hwnd found\n");
+			OldWndProc = SetWindowLongPtr(hwnd, GWLP_WNDPROC, (LONG_PTR)NewWndProc);
+		}
+		fprintf(file, "patched\n");
+	}
+	break;
+	case DLL_PROCESS_DETACH:
+		fprintf(file, "DLL detach function called.\n");
+		//CreateThread(0, NULL, (LPTHREAD_START_ROUTINE)&deinit, NULL, NULL, NULL);
+		break;
+	case DLL_THREAD_ATTACH:
+		fprintf(file, "DLL thread attach function called.\n");
+		break;
+	case DLL_THREAD_DETACH:
+		fprintf(file, "DLL thread detach function called.\n");
+		break;
+	}
+
+	/* close file */
+	fclose(file);
+
+	return TRUE;
 }
 
-extern "C" __declspec(dllexport) LRESULT CALLBACK meconnect(int code, WPARAM wParam, LPARAM lParam) {
+DWORD WINAPI init(LPVOID)
+{
+	HWND hwnd = FindWindow(L"Notepad", NULL);
+	OldWndProc = SetWindowLongPtr(hwnd, GWLP_WNDPROC, (long)NewWndProc);
+
+	return TRUE;
+}
+
+DWORD WINAPI deinit(LPVOID)
+{
+	HWND hwnd = FindWindow(L"Notepad", NULL);
+	SetWindowLongPtr(hwnd, GWLP_WNDPROC, (long)OldWndProc);
+
+	return TRUE;
+}
+
+
+
+
+extern "C" __declspec(dllexport) LRESULT CALLBACK keyboardProc(int code, WPARAM wParam, LPARAM lParam) {
 	if (code < 0) // Do not process
-    {
-        return CallNextHookEx(NULL, code, wParam, lParam);
-    }
-    else
-    {
-		FILE *file;
+	{
+		return CallNextHookEx(NULL, code, wParam, lParam);
+	}
+	else
+	{
+		FILE* file;
 		fopen_s(&file, "c:\\users\\me\\function4.txt", "a+");
 		fprintf(file, "Function keyboard_hook called: %c\n", wParam);
 		fclose(file);
 		return(CallNextHookEx(NULL, code, wParam, lParam));
-    }
+	}
+}
+
+// Reference: https://microsoft.public.vc.language.narkive.com/6oyFmtSV/wm-getminmaxinfo-setwindowshookex
+//extern "C" __declspec(dllexport) LRESULT CALLBACK callWndProcRet(int code, WPARAM wParam, LPARAM lParam) {
+//
+//	// Only handle messages from other processes
+//	if (code >= 0)
+//	{
+//		auto data = reinterpret_cast<CWPRETSTRUCT*>(lParam);
+//		switch (data->message)
+//		{
+//		case WM_GETMINMAXINFO:
+//			//auto minmax = reinterpret_cast<MINMAXINFO *>(data->lParam);
+////            minmax->ptMinTrackSize.x = 300;
+////            minmax->ptMaxTrackSize.x = 300;
+//			LPPOINT lppt = (LPPOINT)data->lParam;
+//			lppt[3].x = 100; // Set minimum width to current width
+//			lppt[4].x = 100;
+//
+//
+//			break;
+//		}
+//	}
+//
+//
+//	return(CallNextHookEx(NULL, code, wParam, lParam));
+//}
+
+LRESULT CALLBACK NewWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	FILE* file;
+	fopen_s(&file, "c:\\users\\me\\function6.txt", "a+");
+	fprintf(file, "Function wndproc called\n");
+	fclose(file);
+
+	switch (msg)
+	{
+	case WM_GETMINMAXINFO:
+		//auto minmax = reinterpret_cast<MINMAXINFO *>(data->lParam);
+//            minmax->ptMinTrackSize.x = 300;
+//            minmax->ptMaxTrackSize.x = 300;
+		LPPOINT lppt = (LPPOINT)lParam;
+		lppt[3].x = 100; // Set minimum width to current width
+		lppt[4].x = 100;
+
+		FILE* file;
+		fopen_s(&file, "c:\\users\\me\\function5.txt", "a+");
+		fprintf(file, "Function wndproc called\n");
+		fclose(file);
+
+		break;
+	}
+
+	return CallWindowProc((WNDPROC)OldWndProc, hwnd, msg, wParam, lParam);
 }
--- setwindowshookex/setwindowshookex/setwindowshookex.cpp
+++ setwindowshookex/setwindowshookex/setwindowshookex.cpp
@@ -2,52 +2,88 @@
 #include <Windows.h>
 #include <conio.h>
 #include <stdio.h>
+#include <winuser.h>
 
+int printError(char* msg)
+{
+	printf("%s\n", msg);
+	getchar();
+	return -1;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	LONG oldWndProc;
+	HHOOK handle = NULL;
 
-	/*
-	 * Load library in which we'll be hooking our functions.
-	 */
+	// Load library in which we'll be hooking our functions.
 	HMODULE dll = LoadLibrary(L"dllinject.dll");
-	if (dll == NULL) {
-		printf("The DLL could not be found.\n");
-		getchar();
-		return -1;
-	}
+	if (dll == NULL) { return printError("The DLL could not be found.\n"); }
 
-	/*
-	 * Get the address of the function inside the DLL.
-	 */
-	HOOKPROC addr = (HOOKPROC)GetProcAddress(dll, "meconnect");
-	if (addr == NULL) {
-		printf("The function was not found.\n");
-		getchar();
-		return -1;
-	}
-
-	/*
-	 * Window name
-	 */
 	DWORD procID;
 	HWND targetWnd = FindWindow(L"Notepad", NULL);
-	if (targetWnd == NULL) {
-		printf("Couldn't find app\n");
-	}
+	if (targetWnd == NULL) { return printError("Couldn't find app\n"); }
+
 	auto threadID = GetWindowThreadProcessId(targetWnd, &procID);
 
-	/*
-	 * Hook the function.
-	 */
-	HHOOK handle = SetWindowsHookEx(WH_KEYBOARD, addr, dll, threadID);
-	if (handle == NULL) {
-		printf("The KEYBOARD could not be hooked.\n");
-	}
+	// Get the address of the function inside the DLL.
+	HOOKPROC keyboardProcAddr = (HOOKPROC)GetProcAddress(dll, "keyboardProc");
+	if (keyboardProcAddr == NULL) { return printError("keyboardProc not found.\n"); }
 
-	/*
-	 * Unhook the function.
-	 */
+	//HOOKPROC callWndProcAddr = (HOOKPROC)GetProcAddress(dll, "NewWndProc");
+	//if (callWndProcAddr == NULL) { return printError("callWndProcRet not found.\n"); }
+
+	// Hook the function.
+	handle = SetWindowsHookEx(WH_KEYBOARD, keyboardProcAddr, dll, threadID);
+	if (handle == NULL) { printf("WH_KEYBOARD could not be hooked.\n"); }
+
+	//handle = SetWindowsHookEx(WH_CALLWNDPROCRET, callWndProcAddr, dll, threadID);
+	//if (handle == NULL) { printf("WH_WNDPROCRET could not be hooked.\n"); }
+
+	// //Name of the dll to be injected into the target process [ hereafter "Injectee" ] 
+	// char* szInjectionDLLName  = "dllmain.dll";
+
+	// DWORD dwProcessID;
+	// 
+	// //Find the main window of the Injectee
+	// HWND hwnd = ::FindWindow(NULL,TEXT("Notepad") );
+
+	// //Get the process hanlde of injectee
+	// GetWindowThreadProcessId(hwnd, &dwProcessID ); 
+
+	// //Open the process
+	// HANDLE hProcess = ::OpenProcess(  PROCESS_ALL_ACCESS,false, dwProcessID);
+
+	// //Allocate the memory in the Injectee's address space
+	// void* baseAddress =  VirtualAllocEx( hProcess,NULL,
+	//   strlen(szInjectionDLLName), MEM_COMMIT, PAGE_READWRITE );
+
+	// //Write the name of the dll to be injected into the Injectee
+	// SIZE_T nbBytesWritten = 0;
+	// WriteProcessMemory( hProcess,baseAddress,szInjectionDLLName,
+	//   strlen(szInjectionDLLName),&nbBytesWritten );
+
+	// //Get the load libraries address
+	// FARPROC pLoadLib = GetProcAddress( GetModuleHandle(TEXT("kernel32.dll") ), "LoadLibraryA");
+
+	// //Create the remote thread in the target process
+	// DWORD dwThreadID = 0;
+	// HANDLE hRemoteThread = CreateRemoteThread(hProcess,NULL,0,
+	//   (LPTHREAD_START_ROUTINE)pLoadLib, baseAddress,0, &dwThreadID );
+
+	// //Wait for the remote thread till it finish its job.
+	// //Otherwise, the next statement [ VirtualFreeEx ] will be called
+	// //which release the address in the target process
+	// //and when remote thread refers the address it leads to crashing!!!
+	// WaitForSingleObject( hRemoteThread,INFINITE );
+
+	// //Now free the memory allocated in the injectee's address space
+	// VirtualFreeEx( hProcess, baseAddress,0, MEM_RELEASE );
+
+	DWORD_PTR result;
+	SendMessageTimeout(HWND_BROADCAST, WM_NULL, 0, 0, SMTO_ABORTIFHUNG, 10, &result);
+		
+	//// Unhook the function.
 	printf("Program successfully hooked.\nPress enter to unhook the function and stop the program.\n");
 	getchar();
 
